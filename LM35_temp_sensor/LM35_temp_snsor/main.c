83cb16bb7a84ccb59bd002bca933ce33d40dac16
Manukrishna M <manumanya637@gmail.com>
2026-05-22 14:34:50 +0530

    second commit

--- LM35_temp_sensor/LM35_temp_snsor/main.c
+++ LM35_temp_sensor/LM35_temp_snsor/main.c
@@ -11,6 +11,31 @@
 #include "lcd.h"
 #include "adc.h"
 
+void my_delay_ms(void)
+{
+	//Step 1 : load 0 to TCNT0
+	TCNT0 = 0;
+	//Step 2 : choosing Normal mode: Mode0
+	TCCR0A &= ~((1<< WGM01) | (1<<WGM00));
+	TCCR0B &= ~(1<< WGM02);
+	//step 3 : Selecting Prescalar value: No Prescaling
+	TCCR0B &= ~((1<< CS02) | (1<< CS01));
+	TCCR0B |= (1<< CS00);
+	/*  Overflow Calculation
+	
+		1fclk = 16MHZ = 16,000,000
+		1 tick = 1/16,000,000 = 62.5ns (nano seconds)
+		For 256 ticks = 256*62.5ns = 16micro seconds(16*10^-6) -> 1 overflow
+		0.5s = 0.5/16micro seconds = 31250 = overflow required for 0.5s delay	 
+	*/
+	for(uint16_t i=0;i<31250;i++)
+	{
+		// Overflow check command
+		while(!(TIFR0 & (1<<TOV0)));
+		TIFR0 |= (1<<TOV0); // Clearing the bit by writing to 1
+	}
+}
+
 void uart_init()
 {
 	/*Step 1: Load baud rate : 9600*/
@@ -37,6 +62,14 @@ void uart_transmit(char data)
 	UDR0 = data;
 }
 
+void uart_TxString(const char *str)
+{
+	while (*str)
+	{
+		uart_transmit(*str++);
+	}
+}
+
 int main(void)
 {
 	uint16_t adc_value;
@@ -48,6 +81,8 @@ int main(void)
     adc_init();
 	lcd_init();
 	uart_init();
+	my_delay_ms();
+	
     while (1) 
     {
 		adc_value = read_adc(0);
@@ -62,15 +97,17 @@ int main(void)
 		lcd_print_float(temperature);
 		lcd_data(0xDF); //Hex Value for degree Symbol
 		lcd_print("C");
-		_delay_ms(500);
+		my_delay_ms();
 		
 		//UART Transmission
-		uart_transmit('\n');
-		_delay_ms(500);
+		char temp_buffer[10];
+		dtostrf(temperature,4,1,temp_buffer);   // convert to string 
+		uart_TxString(temp_buffer);
+		uart_TxString("C\r\n");
+		my_delay_ms();
 		
 		//LED Blink
 		PORTB ^= (1<<PORTB0);
-		_delay_ms(500);
+		my_delay_ms();
     }
 }
-
